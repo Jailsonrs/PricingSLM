8b19b70cfd0f1457e3331a0ec650570d431a89f9
Jailsonrs <jailsonr@alu.ufc.br>
2025-01-29 19:29:40 -0300

    updated etl

--- models/etl.py
+++ models/etl.py
@@ -4,24 +4,18 @@ import statsmodels
 
 df = pd.read_csv("DATA/raw/products.csv",sep=";")
 
-<<<<<<< HEAD
 print(df.head(10))
 
 df.info()
-=======
 #print(df.head(10))
 
 #df.info()
->>>>>>> 0d06222 (models and utils)
-
 
 #obtendo somente colunas necessarias
 # criar variavel sintética de data de compra 
 # extrair ano, semana, trimestre.
 # simular demanda diária => derivar a partir dessas a demanda para semana, trimestre etc...
-<<<<<<< HEAD
 #modelar.
-=======
 #modelar.
 
 train_cols = ['PRODUTO',"CLASSE TERAPÊUTICA","LABORATÓRIO","TARJA","PMC Sem Imposto","PMC 0%","PMC 12%","PMC 18%","PMC 21%","PMC 22%"]
@@ -40,7 +34,6 @@ df_train[pmc_columns] = df_train[pmc_columns].apply(lambda col: col.str.replace(
 
 print(df_train.info())
 
-
 # Step 1: Generate date range
 date_range = pd.date_range(start="2023-01-01", end="2024-12-31", freq="D")
 
@@ -53,20 +46,20 @@ df_expanded = expanded_data.merge(df_train, on="PRODUTO", how="left")
 
 # Step 4: Add synthetic QUANTITY_SOLD column
 
-
 np.random.seed(42)  # For reproducibility
-df_expanded["QUANTITY_SOLD"] = np.random.exponential(1, 50, size=len(df_expanded))  # Random quantity between 1 and 50
+#df_expanded["QUANTITY_SOLD"] = np.random.exponential(60, size=len(df_expanded))  # Random quantity between 1 and 50
 
+print('random prices')
 np.random.exponential()
 df_expanded["UNITARY_PRICE"] = df_expanded.apply(
     lambda row: np.random.uniform(row["PMC 0%"], row["PMC 22%"]), axis=1
 )
-
+print('random prices end...')
 # Display the result
+print("writing to file...")
 print(df_expanded.head())
 
 df_expanded.to_csv("DATA/processed/synthetic_sales_expanded.csv", index=False)   
 
 
 
->>>>>>> 0d06222 (models and utils)
